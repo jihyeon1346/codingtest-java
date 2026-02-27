296c92a8d0a426172c1495ebd6d23d53a8371115
jihyeon <yio1346@gmail.com>
2026-02-27 10:14:05 +0900

    temp: remove programmers directory

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(int n) {
-        for (int x = 1; x <= n; x ++){
-            if(n % x == 1)
-                return x;
-        }
-        return 0;
-    }
-}
